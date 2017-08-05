dd2f428fc51cf91c5bc478383d5712193d719f78
pathakms <pathakabhishek3007@gmail.com>
2017-08-05 13:23:39 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -8,4 +8,3 @@ globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
-Contact GitHub 
